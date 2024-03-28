f96b3b2b60f939adf37558b589c9102a6adb735a
John Guerrero <ingjohnguerrero@gmail.com>
2024-03-28 18:20:14 -0500

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by John Edwin Guerrero Ayala on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
