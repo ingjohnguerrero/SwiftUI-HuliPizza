c6eb1169b76d06f78a08d19b8c67d29615314d1d
John Guerrero <ingjohnguerrero@gmail.com>
2024-09-11 20:40:54 -0500

    feat (HeaderView): Add environment variable

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -41,6 +41,7 @@ struct ContentView: View {
         }
         .padding()
         .background(.linearGradient(colors: [.cyan, Color("Surf"), Color("Sky"), .white], startPoint: .topLeading, endPoint: .bottom))
+        .environmentObject(orders)
     }
 }
 
--- HuliPizza/OrderView.swift
+++ HuliPizza/OrderView.swift
@@ -28,12 +28,6 @@ struct OrderView: View {
                     Text("Order Pizza")
                         .font(.title)
                     Spacer()
-                    Label {
-                        Text(orders.orderTotal, format: .currency(code: "USD"))
-                    }
-                    icon: {
-                        Image(systemName: orders.orderItems.isEmpty ? "cart" : "cart.circle.fill")
-                    }
                 }
                 .padding([.leading, .trailing], 15)
                 .padding([.top, .bottom], 10)
